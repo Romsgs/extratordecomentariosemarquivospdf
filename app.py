928b6ab910c7ff35bb58ae5d0994a6b578c9400d
Romsgs <rsalobrena@gmail.com>
2024-05-22 14:13:03 -0300

    :tada: first comit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import fitz  # PyMuPDF
 from io import BytesIO
 import re
-
+from datetime import datetime 
 st.set_page_config(page_title="Extração de Comentários")
 
 def extract_revisoes_table(text):
@@ -31,7 +31,7 @@ def extract_comments_from_pdf(pdf_file):
     numero_documento_blossom = numero_documento_blossom.group() if numero_documento_blossom else "Não encontrado"
 
     tabela_revisoes = extract_revisoes_table(text)
-    df_revisoes = tabela_revisoes if not tabela_revisoes.empty else pd.DataFrame(columns=["Rev.", "Tipo", "Descrição", "Elab.", "Ver.", "Apr.", "Aut.", "Data"])
+    df_revisoes = tabela_revisoes if not tabela_revisoes.empty else pd.DataFrame(columns=["Rev.", "T.E", "Descrição", "Elab.", "Ver.", "Apr.", "Aut.", "Data"])
 
     for page_num in range(len(doc)):
         page = doc[page_num]
@@ -51,9 +51,11 @@ def organize_comments(documento_cliente, documento_blossom, comments):
     }
     return pd.DataFrame(data)
 
-def save_to_excel(tables: pd.DataFrame, filename):
+def save_to_excel(tables, filename):
     with BytesIO() as buffer:
-        tables.to_excel(filename)
+        with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+            for i, table in enumerate(tables):
+                table.to_excel(writer, index=False, sheet_name=f"Tabela {i+1}")
         st.download_button(label="Download Excel", data=buffer.getvalue(), file_name=f"{filename}.xlsx")
 
 uploaded_files = st.file_uploader("Faça o upload de arquivos", type=['pdf', 'dwg'], accept_multiple_files=True)
@@ -69,20 +71,23 @@ if uploaded_files:
             table = organize_comments(numero_documento_cliente, numero_documento_blossom, comments)
             table["Revisão do Documento do Cliente"] = df_revisoes['Rev.'].iloc[-1] if not df_revisoes.empty else "Não encontrado"
             all_comments.append(table)
-        
+            all_revisoes.append(df_revisoes)
 
     if all_comments:
         combined_comments = pd.concat(all_comments, ignore_index=True)
-        
+        combined_revisoes = pd.concat(all_revisoes, ignore_index=True)
         
         st.write("Comentários extraídos:")
         st.dataframe(combined_comments)
 
-
+        st.write("Tabela de Revisões:")
+        st.dataframe(combined_revisoes)
+        nome_Arquivo = st.text_input(label='Nome do arquivo:')
+        data_atual = datetime.today().strftime('%d/%m/%Y-%H:%M')
         if st.button("Exportar para Excel"):
-            filename = st.text_input("Digite o nome do arquivo:", value="Comentarios")
+            filename = st.text_input("Digite o nome do arquivo:", value=nome_Arquivo +"_"+ data_atual)
             if filename:
-                save_to_excel(combined_comments, filename)
+                save_to_excel([combined_comments, combined_revisoes], filename)
             else:
                 st.warning("Por favor, forneça um nome para o arquivo.")
     else:
@@ -90,4 +95,3 @@ if uploaded_files:
 
 if st.button("Resetar"):
     st.experimental_rerun()
-
